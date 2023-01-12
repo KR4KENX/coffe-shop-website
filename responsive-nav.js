a397d5aff77c3e0f289e30bcf5eefe15296cc4be
adriank64216 <sqat1024@gmail.com>
2023-01-12 13:49:32 +0100

    new mobile menu design

--- responsive-nav.js
+++ responsive-nav.js
@@ -5,71 +5,92 @@
         let message = document.getElementsByClassName("add-to-cart-info")[0];
         let messageInterval = setInterval(() => {
             message.style.opacity = "0";
+            message.style.display = "none";
         }, 3000);
         
         inputButtonFirst.addEventListener("click", () => {
+            message.style.display = "block";
             message.style.opacity = "1";
+            
             messageInterval;
         });
         inputButtonSecond.addEventListener("click",() => {
+            message.style.display = "block";
             message.style.opacity = "1";
+            
             messageInterval;
         });
         inputButtonThird.addEventListener("click",() => {
+            message.style.display = "block";
             message.style.opacity = "1";
+            
             messageInterval
         });
 
         //Mobile navbar
         const mobileScreenConfigMenuOn = {
-            nav: "width: 100%; height: 100vh; position: fixed; z-index: 2; top: 0px; left: 0px;",
+            nav: "width: 100%; height: 100vh; position: fixed; z-index: 2; top: 0; left: 0; display: flex; justify-content: center; align-items: center; flex-direction: column;",
+            navMenu: "display: block; margin: 0",
             img: "display: none;",
-            ul: "display: block;",
+            ul: "width: 100%;",
             ulLi: "display: block;",
-            ulLiA: "font-size: 1.5em; line-height: 3.5em;",
-            input: "color: white; display: block;position:absolute; top: .5em; right: .8em;font-size: 3em;background: none;border: none;"
+            ulLiA: "font-size: 1em; line-height: 2.5em;",
         }
         const mobileScreenConfigMenuOff = {
-            nav: "width: 100%; height: 15vh; position: fixed; z-index: 2; top: 0px; left: 0px;background-color: rgba(194, 155, 65);",
+            nav: "width: 100%; height: 15vh; position: fixed; z-index: 2; top: 0; left: 0;background-color: rgba(194, 155, 65);",
+            navMenu: "display: block; position: static;",
             ul: "display: none;",
-            input: "color: white; display: block; margin-right: .5em; font-size: 3em;background: none;border: none; font-family: 'Montserrat', sans-serif;",
             img: "display: block;"
         };
         let button = document.getElementsByTagName("input")[0];
         let nav = document.getElementsByTagName("nav")[0];
-        let navMenu = nav.getElementsByTagName("input")[0];
+        let navMenu = nav.querySelector("#navMenu");
         let ul = nav.getElementsByTagName("ul")[0];
         let ulLi = ul.getElementsByTagName("li");
         let ulLiA = nav.getElementsByTagName("a");
         let img = nav.getElementsByTagName("img")[0]
         let isMenuVisible = false;
 
+        function navMenuIconHandler(){
+            navMenu.classList.toggle("active");
+            //if menuvisible {!menuvisible}
+            menu();
+            if(isMenuVisible) {isMenuVisible = false; return;}
+            if(!isMenuVisible) {isMenuVisible = true;}
+        }
+
+        navMenu.addEventListener("click", () => navMenuIconHandler());
+
         if (window.innerWidth <= 650) {
-            console.log("okno")
+            navMenu.style.display = "block";
             nav.style.cssText = mobileScreenConfigMenuOff["nav"];
             ul.style.cssText = mobileScreenConfigMenuOff["ul"];
-            navMenu.style.cssText = mobileScreenConfigMenuOff["input"];
-            navMenu.value = "MENU";
+            //navMenu.style.cssText = mobileScreenConfigMenuOff["input"];
+            //navMenu.value = "MENU";
             img.style.cssText = mobileScreenConfigMenuOff["img"];
         }
         function menu() {
             if (window.innerWidth > 650) return;
-            navMenu.value == "X" ? isMenuVisible = true : isMenuVisible = false;
+            //navMenu.value == "X" ? isMenuVisible = true : isMenuVisible = false;
+            console.log(isMenuVisible);
 
-            console.log("klik" + isMenuVisible);
             if (isMenuVisible) {
                 nav.style.cssText = mobileScreenConfigMenuOff["nav"];
+                navMenu.style.cssText = mobileScreenConfigMenuOff["navMenu"];
                 ul.style.cssText = mobileScreenConfigMenuOff["ul"];
-                navMenu.style.cssText = mobileScreenConfigMenuOff["input"];
-                navMenu.value = "MENU";
+                //navMenu.style.cssText = mobileScreenConfigMenuOff["input"];
+                //navMenu.value = "MENU";
                 img.style.cssText = mobileScreenConfigMenuOff["img"];
+                //remove class active from navmenuIcon
+                navMenu.classList.remove("active");
             }
             if (!isMenuVisible) {
-                navMenu.value = "X";
+               // navMenu.value = "X";
                 nav.style.cssText = mobileScreenConfigMenuOn["nav"];
+                navMenu.style.cssText = mobileScreenConfigMenuOn["navMenu"];
                 ul.style.cssText = mobileScreenConfigMenuOn["ul"];
                 img.style.cssText = mobileScreenConfigMenuOn["img"];
-                navMenu.getElementsByTagName("input")[0].style.cssText = "display: block;";
+               // navMenu.getElementsByTagName("input")[0].style.cssText = "display: block;";
 
                 for (let i = 0; i < ulLi.length; i++) {
                     ulLi[i].style.cssText = mobileScreenConfigMenuOn["ulLi"]
@@ -80,4 +101,5 @@
                     ulLiA[i].style.cssText = mobileScreenConfigMenuOn["ulLiA"];
                 }
             }
+            console.log(isMenuVisible);
         }
